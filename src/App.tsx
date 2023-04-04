8e003a24fc52c81f337e46f1089ed5cac50fc4bf
kkpagaev <kkpagaev@gmail.com>
2023-04-05 02:36:09 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      Hello, world!<Button>Click me!</Button>
+    </div>
+  )
+}
+
+export default App
